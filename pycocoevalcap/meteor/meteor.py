61a793e162e96fcb172f15864d5a05ec7650a2bb
Vargnatt <kudosblade@gmail.com>
2018-01-04 17:30:10 +0800

    ??

--- pycocoevalcap/meteor/meteor.py
+++ pycocoevalcap/meteor/meteor.py
@@ -7,6 +7,7 @@ import os
 import sys
 import subprocess
 import threading
+import code
 
 # Assumes meteor-1.5.jar is in the same directory as meteor.py.  Change as needed.
 METEOR_JAR = 'meteor-1.5.jar'
@@ -35,10 +36,12 @@ class Meteor:
         for i in imgIds:
             assert(len(res[i]) == 1)
             stat = self._stat(res[i][0], gts[i])
-            eval_line += ' ||| {}'.format(stat)
+            eval_line += ' ||| {}'.format(stat.decode())
 
-        self.meteor_p.stdin.write('{}\n'.format(eval_line))
+        self.meteor_p.stdin.write('{}\n'.format(eval_line).encode())
+        self.meteor_p.stdin.flush()
         for i in range(0,len(imgIds)):
+            # code.interact(local=locals())
             scores.append(float(self.meteor_p.stdout.readline().strip()))
         score = float(self.meteor_p.stdout.readline().strip())
         self.lock.release()
@@ -52,18 +55,20 @@ class Meteor:
         # SCORE ||| reference 1 words ||| reference n words ||| hypothesis words
         # if not isinstance(hypothesis_str,str):
         #     hypothesis_str=str(hypothesis_str)
-        hypothesis_str = hypothesis_str.replace(b'|||','').replace(b'  ',b' ')
+        hypothesis_str = hypothesis_str.replace(b'|||',b'').replace(b'  ',b' ')
         score_line = b' ||| '.join((b'SCORE', b' ||| '.join(reference_list), hypothesis_str))
         score_line=str(score_line,'utf-8')
-        self.meteor_p.stdin.write('{}\n'.format(score_line))
+        # code.interact(local=locals())
+        self.meteor_p.stdin.write('{}\n'.format(score_line).encode())
+        self.meteor_p.stdin.flush()
         return self.meteor_p.stdout.readline().strip()
 
     def _score(self, hypothesis_str, reference_list):
         self.lock.acquire()
         # SCORE ||| reference 1 words ||| reference n words ||| hypothesis words
-        hypothesis_str = hypothesis_str.replace('|||','').replace('  ',' ')
-        score_line = ' ||| '.join(('SCORE', ' ||| '.join(reference_list), hypothesis_str))
-        self.meteor_p.stdin.write('{}\n'.format(score_line))
+        hypothesis_str = hypothesis_str.replace(b'|||',b'').replace(b'  ',b' ')
+        score_line = b' ||| '.join((b'SCORE', b' ||| '.join(reference_list), hypothesis_str))
+        self.meteor_p.stdin.write('{}\n'.format(score_line.decode("utf-8")).encode())
         stats = self.meteor_p.stdout.readline().strip()
         eval_line = 'EVAL ||| {}'.format(stats)
         # EVAL ||| stats 
